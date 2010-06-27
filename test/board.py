b7655d36c656132f172455dd4695fc26940d975d
mike o'brien <mike@meetup.com>
2010-06-27 12:52:05 -0400

    testings

--- test/board.py
+++ test/board.py
@@ -1,7 +1,8 @@
 from java.util import ArrayList
 from skrib.util import TrieNode
-#tn = TrieNode.loadDictionary("/home/mike/WORD.LST");
+tn = TrieNode.loadDictionary("/home/mike/WORD.LST");
 
+#TODO fix test
 gd = injector.getInstance(GameDAO)
 g = gd.createNewGame(None,2)
 #g2 = gd.get(g.getId())
@@ -10,8 +11,8 @@ tp.add( TilePlacement(Tiles.LetterTile.A,GridPosition(7,7) ))
 tp.add( TilePlacement(Tiles.LetterTile.S,GridPosition(7,8) ))
 tp.add( TilePlacement(Tiles.LetterTile.S,GridPosition(7,9) ))
 m = Move(tp)
-print "turn number: ", g.getTurnNumber(), "player turn: ", g.getNextTurnPlayerNum()
-print "making move"
+#print "turn number: ", g.getTurnNumber(), "player turn: ", g.getNextTurnPlayerNum()
+#print "making move"
 g.makeMove(m, tn)
 gd.postMoveUpdate(g)
 #print g.getBoardState().toString()
